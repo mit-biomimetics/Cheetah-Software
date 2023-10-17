52619d43c9086d5e9123ea6975b8fe57cb4dad98
davood-dorostkar <dado2hacker@gmail.com>
2023-10-17 07:52:09 +0330

    fix: cmake issues with google test repo download, qt5 path finding

--- common/DownloadProject.cmake
+++ common/DownloadProject.cmake
@@ -79,7 +79,7 @@
 #   include(DownloadProject)
 #   download_project(PROJ                googletest
 #                    GIT_REPOSITORY      https://github.com/google/googletest.git
-#                    GIT_TAG             master
+#                    GIT_TAG             main
 #                    UPDATE_DISCONNECTED 1
 #                    QUIET
 #   )
